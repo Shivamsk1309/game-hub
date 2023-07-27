2ca25db62bb34e97cc31b8a011b274d419dce871
Shivam Kaushik <skshivam.1309@gmail.com>
2023-07-27 18:51:19 +0530

    handle games with no image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text, Textarea } from "@chakra-ui/react";
-import logo from "../assets/icons8-chakra-ui.svg";
+import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image boxSize="60px" src={logo} />
+      <Image boxSize="80px" src={logo} />
+      
 
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
